d8a4f82b7c5f1066cf609d01e67eebe290e183b2
K-TESLIM <kamaldeenteslim@gmail.com>
2023-10-27 10:20:38 +0100

    Using "Classes"

--- src/main/kotlin/MyProject.kt
+++ src/main/kotlin/MyProject.kt
@@ -726,7 +726,7 @@ fun main() {
 }*/
 
 //Practice
-fun printSurrounded(value: String = "", prefix: String = "<", postfix: String = ">") {
+/*fun printSurrounded(value: String = "", prefix: String = "<", postfix: String = ">") {
     println("$prefix$value$postfix")
 }
 
@@ -734,5 +734,30 @@ fun printSurrounded(value: String = "", prefix: String = "<", postfix: String = 
 fun main() {
     printSurrounded("ABC", ".")
     printSurrounded(postfix = ".")
+}*/
+
+
+//using Classes
+class Subject(
+    val name: String,
+    val teacher: Teacher,
+    val isObligatory: Boolean,
+)
+
+class Teacher(
+    val name: String,
+    val surname: String,
+    val birthday: String,
+    val status: String,
+)
+
+fun main() {
+    // I use named arguments convention here
+    val remigiuszFrost =
+        Teacher(name = "Remigiusz", surname = "Frost", birthday = "23.07.1987", status = "ACTIVE")
+    val biology1 = Subject(name = "Biology 1", teacher = remigiuszFrost, isObligatory = true)
+    println(biology1.isObligatory) // true
+    println(biology1.teacher.birthday) // 23.07.1987
+    println(biology1.teacher.status) // ACTIVE
 }
 
